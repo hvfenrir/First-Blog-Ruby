1f0a965fbee7e0c1cb781c036479a94f7b7ef0e6
hvfenrir <pqvinh.207@gmail.com>
2016-02-29 23:10:12 +0700

    Update img.gif & fix bug

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,6 @@ class Article < ActiveRecord::Base
  
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true
-  validates :comment, presence: true
   # It returns the articles whose titles contain one or more words that form the query
   def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
